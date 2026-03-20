3fa9ea1f654fa778cb3bb6b436d62ba4330ba701
buchhavee <mach0015@stud.kea.dk>
2026-03-20 14:26:48 +0100

    fix: correct comment for syncTouch option in SmoothScroll component

--- components/SmoothScroll.tsx
+++ components/SmoothScroll.tsx
@@ -23,7 +23,7 @@ export default function SmoothScroll({ children }: SmoothScrollProps) {
       duration: 1.2,
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
       smoothWheel: true,
-      smoothTouch: false, // Native scroll on iOS – required for Safari address bar transparency
+      syncTouch: false, // Native scroll on iOS – required for Safari address bar transparency (default, but explicit)
     });
 
     lenisRef.current = lenis;
